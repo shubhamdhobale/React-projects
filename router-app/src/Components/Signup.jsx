24a8d565b06b374696713b1a7f26fb32a2b3da24
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2023-10-13 21:53:14 +0530

    router-app

--- router-app/src/Components/Signup.jsx
+++ router-app/src/Components/Signup.jsx
@@ -4,16 +4,16 @@ import Template from './Template';
 
 
 function Signup({ setIsLoggedIn }) {
-  return (
-    <Template
-      title="Join the millions learning to code with StudyNotion for free"
-      desc1="Build skills for today, tomorrow, and beyond."
-      desc2="Education to future-proof your career."
-      image={signupImg}
-      formType="signup"
-      setIsLoggedIn={setIsLoggedIn}
-    />
-  );
+    return ( <
+        Template
+        // title="Join the millions learning to code with StudyNotion for free"
+        // desc1 = "Build skills for today, tomorrow, and beyond."
+        // desc2 = "Education to future-proof your career."
+        image = { signupImg }
+        formType = "signup"
+        setIsLoggedIn = { setIsLoggedIn }
+        />
+    );
 }
 
 export default Signup
