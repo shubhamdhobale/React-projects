7d437e36ac0c487151f39c2644beb299b2256517
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-04-05 18:58:38 +0530

    comiit

---
+++ passwordgenerator/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-black">
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
