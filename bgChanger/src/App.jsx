1364e2251927db624edbd33caeb5a27545b8deab
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-04-05 10:48:27 +0530

    comit

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-4xl font-bold'>Bg-Changer</h1>
+      <h1 className='text-2xl font-bold '>Bg-Changer</h1>
     </div>
   )
 }
