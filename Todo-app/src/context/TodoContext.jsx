368f5ee0f107a7b7dae6ff3137e2f05bb1d7e870
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-04-17 11:45:09 +0530

    Todo app Project setup

---
+++ Todo-app/src/context/TodoContext.jsx
@@ -0,0 +1,19 @@
+import {createContext , useContext} from 'react';
+
+export const TodoContext = createContext({
+  todos: [
+    {
+    id : 1,
+    todo : "todo msg",
+    completed: false,
+    }
+  ],
+  addTodo : (todo) => {}
+})
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+}
+
+
+export const TodoProvider = TodoContext.Provider
