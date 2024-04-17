1735f7e368162477999a0e8769a10838a7e1476d
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-04-17 20:04:26 +0530

    complete redux toolkit functionality with todo app

--- reduxtoolkit-Todo/src/App.jsx
+++ reduxtoolkit-Todo/src/App.jsx
@@ -1,11 +1,14 @@
-
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-
+  
   return (
     <>
-     <h1>Todo App with Redux ToolKit</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
