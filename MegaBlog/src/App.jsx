df9d8b41b1b8fc9845339e4fb5db08d3dc616a96
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-04-20 12:15:58 +0530

    initialization of project and configuring appwrite and env file

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>Blog App With Appwrite</h1>
+    </>
+  )
+}
+
+export default App
